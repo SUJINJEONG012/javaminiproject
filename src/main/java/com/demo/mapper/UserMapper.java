e53aec86d30aa32b3a8338f5da0a39e1326e6671
SUJINJEONG012 <peekaboo32@naver.com>
2023-01-02 01:09:49 +0900

    회원정보 디비에저장하는 로직

--- src/main/java/com/demo/mapper/UserMapper.java
+++ src/main/java/com/demo/mapper/UserMapper.java
@@ -10,7 +10,7 @@ public interface UserMapper {
  @Select("select user_name " + "from user_table "+ "where user_id = #{user_id}")
  String checkUserIdExist(String user_id);
  
- @Insert("insert into user_table (user_idx, user_name, user_id,user_pw)" + 
+ @Insert("insert into user_table (user_name, user_id, user_pw)" + 
   "values ( #{user_name}, #{user_id}, #{user_pw})") 
  void addUserInfo(UserBean joinUserBean);
  
